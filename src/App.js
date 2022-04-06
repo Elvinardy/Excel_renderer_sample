a36be840eef1e1071ccc20e90d157a163ed9cbb5
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-04-06 11:37:59 +0800

    working sample

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import './App.css';
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
+import { Jumbotron, Col, Input, InputGroup, InputGroupAddon, FormGroup, Label, Button, Fade, FormFeedback, Container, Card } from 'reactstrap';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      isOpen: false,
+      dataLoaded: false,
+      isFormInvalid: false,
+      rows: null,
+      cols: null
+    }
+    this.fileHandler = this.fileHandler.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.openFileBrowser = this.openFileBrowser.bind(this);
+    this.renderFile = this.renderFile.bind(this);
+    this.fileInput = React.createRef();
+  }
+
+  renderFile = (fileObj) => {
+      //just pass the fileObj as parameter
+      ExcelRenderer(fileObj, (err, resp) => {
+        if(err){
+          console.log(err);            
+        }
+        else{
+          this.setState({
+            dataLoaded: true,
+            cols: resp.cols,
+            rows: resp.rows
+          });
+        }
+      }); 
+  }
+
+  fileHandler = (event) => {    
+    if(event.target.files.length){
+      let fileObj = event.target.files[0];
+      let fileName = fileObj.name;
+
+      
+      //check for file extension and pass only if it is .xlsx and display error message otherwise
+      if(fileName.slice(fileName.lastIndexOf('.')+1) === "xlsx"){
+        this.setState({
+          uploadedFileName: fileName,
+          isFormInvalid: false
+        });
+        this.renderFile(fileObj)
+      }    
+      else{
+        this.setState({
+          isFormInvalid: true,
+          uploadedFileName: ""
+        })
+      }
+    }               
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  openFileBrowser = () => {
+    this.fileInput.current.click();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Jumbotron className="jumbotron-background">          
+              <h1 className="display-3">react-excel-renderer</h1>
+              <p className="lead">Welcome to the demo of react-excel-renderer.</p>             
+              <hr className="my-2" />
+          </Jumbotron>
+        </div>
+        <Container>
+        <form>
+          <FormGroup row>       
+            <Col xs={8} sm={16} lg={20}>                                                     
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <Button color="info" style={{color: "white", zIndex: 0}} onClick={this.openFileBrowser.bind(this)}><i className="cui-file"></i>Upload File</Button>
+                  <input type="file" hidden onChange={this.fileHandler.bind(this)} ref={this.fileInput} onClick={(event)=> { event.target.value = null }} style={{"padding":"10px"}} />                                
+                </InputGroupAddon>
+                <Input type="text" className="form-control" value={this.state.uploadedFileName} readOnly invalid={this.state.isFormInvalid} />                                              
+                <FormFeedback>    
+                  <Fade in={this.state.isFormInvalid} tag="h6" style={{fontStyle: "italic"}}>
+                    Please select a .xlsx file only !
+                  </Fade>                                                                
+                </FormFeedback>
+              </InputGroup>     
+            </Col>                                                   
+          </FormGroup>   
+        </form>
+
+        {this.state.dataLoaded && 
+        <div>
+          <Card body outline color="secondary" className="restrict-card">
+            
+              <OutTable data={this.state.rows} columns={this.state.cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
+            
+          </Card>  
+        </div>}
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
